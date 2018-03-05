59b5f3c400789aa0e5dd73d1a0420b0a4b8599d2
Josh Remaley <joshremaley@gmail.com>
2018-03-05 17:01:44 -0600

    hello world route

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', function(req,res,next){
+    res.send('Hello world');
+})
+
+
+app.listen(3002, () => console.log('server listening'));
