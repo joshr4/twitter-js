a9f8587bcfa26d805afb16ad7bdcdb8ace451e31
Josh Remaley <joshremaley@gmail.com>
2018-03-05 17:12:38 -0600

    log all gets

--- app.js
+++ app.js
@@ -6,5 +6,12 @@ app.get('/', function(req,res,next){
     res.send('Hello world');
 })
 
+app.get('/news', function(req,res,next){
+    res.send('news!');
+})
 
-app.listen(3002, () => console.log('server listening'));
+app.use(function(req,res,next){
+    console.log(req.url)
+;})
+
+app.listen(3004, () => console.log('server listening'));
